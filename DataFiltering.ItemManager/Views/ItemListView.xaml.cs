b65bd6edc21a43ce78ac3dcd97c1550bbe451201
Jazz Adrias <cerberus.outlaw@gmail.com>
2024-11-25 15:55:10 +1100

    minor refactoring

--- DataFiltering.ItemManager/Views/ItemListView.xaml.cs
+++ DataFiltering.ItemManager/Views/ItemListView.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,7 +9,7 @@ namespace DataFiltering.ItemManager.Views
     /// <summary>
     /// Interaction logic for ItemListView.xaml
     /// </summary>
-    public partial class ItemListView : UserControl, INotifyPropertyChanged
+    public partial class ItemListView : UserControl
     {
         private string _filterText = string.Empty;
 
@@ -25,22 +24,12 @@ namespace DataFiltering.ItemManager.Views
             propertyType: typeof(ObservableCollection<string>),
             ownerType: typeof(ItemListView),
             typeMetadata: new FrameworkPropertyMetadata(default(ObservableCollection<string>), OnItemsSourceChanged));
-
-        private static void OnItemsSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            var control = (ItemListView)d;
-            control.ItemsCollectionView = CollectionViewSource.GetDefaultView(control.ItemsSource);
-            control.ItemsCollectionView.Filter = control.FilteredItems;
-        }
-
         public ObservableCollection<string> ItemsSource
         {
             get => (ObservableCollection<string>)GetValue(ItemSourceProperty);
             set => SetValue(ItemSourceProperty, value);
         }
-
         public ICollectionView? ItemsCollectionView { get; private set; }
-
         public string FilterText
         {
             get => _filterText;
@@ -50,7 +39,6 @@ namespace DataFiltering.ItemManager.Views
                     ItemsCollectionView?.Refresh();
             }
         }
-
         private bool FilteredItems(object obj)
         {
             if (string.IsNullOrWhiteSpace(FilterText))
@@ -59,77 +47,11 @@ namespace DataFiltering.ItemManager.Views
             return obj is string str && str.ToLower().Contains(FilterText.ToLower());
         }
 
-
-        /// <summary>
-        /// Occurs when a property value changes.
-        /// </summary>
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        /// <summary>
-        /// Checks if a property already matches a desired value. Sets the property and
-        /// notifies listeners only when necessary.
-        /// </summary>
-        /// <typeparam name="T">Type of the property.</typeparam>
-        /// <param name="storage">Reference to a property with both getter and setter.</param>
-        /// <param name="value">Desired value for the property.</param>
-        /// <param name="propertyName">Name of the property used to notify listeners. This
-        /// value is optional and can be provided automatically when invoked from compilers that
-        /// support CallerMemberName.</param>
-        /// <returns>True if the value was changed, false if the existing value matched the
-        /// desired value.</returns>
-        protected virtual bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string? propertyName = null)
+        private static void OnItemsSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            if (EqualityComparer<T>.Default.Equals(storage, value)) return false;
-
-            storage = value;
-            RaisePropertyChanged(propertyName);
-
-            return true;
-        }
-
-        /// <summary>
-        /// Checks if a property already matches a desired value. Sets the property and
-        /// notifies listeners only when necessary.
-        /// </summary>
-        /// <typeparam name="T">Type of the property.</typeparam>
-        /// <param name="storage">Reference to a property with both getter and setter.</param>
-        /// <param name="value">Desired value for the property.</param>
-        /// <param name="propertyName">Name of the property used to notify listeners. This
-        /// value is optional and can be provided automatically when invoked from compilers that
-        /// support CallerMemberName.</param>
-        /// <param name="onChanged">Action that is called after the property value has been changed.</param>
-        /// <returns>True if the value was changed, false if the existing value matched the
-        /// desired value.</returns>
-        protected virtual bool SetProperty<T>(ref T storage, T value, Action? onChanged,
-            [CallerMemberName] string? propertyName = null)
-        {
-            if (EqualityComparer<T>.Default.Equals(storage, value)) return false;
-
-            storage = value;
-            onChanged?.Invoke();
-            RaisePropertyChanged(propertyName);
-
-            return true;
-        }
-
-        /// <summary>
-        /// Raises this object's PropertyChanged event.
-        /// </summary>
-        /// <param name="propertyName">Name of the property used to notify listeners. This
-        /// value is optional and can be provided automatically when invoked from compilers
-        /// that support <see cref="CallerMemberNameAttribute"/>.</param>
-        protected void RaisePropertyChanged([CallerMemberName] string? propertyName = null)
-        {
-            OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
-        }
-
-        /// <summary>
-        /// Raises this object's PropertyChanged event.
-        /// </summary>
-        /// <param name="args">The PropertyChangedEventArgs</param>
-        protected virtual void OnPropertyChanged(PropertyChangedEventArgs args)
-        {
-            PropertyChanged?.Invoke(this, args);
+            var control = (ItemListView)d;
+            control.ItemsCollectionView = CollectionViewSource.GetDefaultView(control.ItemsSource);
+            control.ItemsCollectionView.Filter = control.FilteredItems;
         }
     }
 }
