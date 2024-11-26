36915ad6142200499d57105318f530584a4afd40
Jazz Adrias <cerberus.outlaw@gmail.com>
2024-11-26 14:17:32 +1100

    Added FilterTextValidation Rule

---
+++ DataFiltering.Core/Styles/ValidationRules/FilterTextLengthRule.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace DataFiltering.Shared.Styles.ValidationRules
+{
+    public class FilterTextLengthRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value is string text && text.Length > 20)
+            {
+                return new ValidationResult(false, "Filter Text exceeding limit of 20 characters");
+            }
+
+            return ValidationResult.ValidResult;
+        }
+    }
+}
